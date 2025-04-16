9a226319d2543b96f910b2ccdaee489993b700e5
Sree Harsha C S <sreeharshacs@gmail.com>
2025-04-15 21:53:51 -0700

    Disable python tools which is delaying claude stuff

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -13,6 +13,7 @@ return {
             -- },
             -- default_provider = "ollama",
         },
+        disabled_tools = { "python" },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
         build = "make",
         -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
